4b30b46391abd8a23307919cbfd77fbe8d5ada6f
许飞 <feixu@tju.edu.cn>
2017-08-13 16:41:08 +0800

    posts index vue实现

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,10 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   # GET /posts/1
