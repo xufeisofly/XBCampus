f610d26f77bb222353c41fb652c7001349623c1a
linliulynn <linliulynn@gmail.com>
2017-10-18 23:39:14 -0400

    add comment form in post page

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment].permit(:content))
+    @comment.user_id = current_student.id if current_student
+    @comment.save
+
+    if @comment.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018202851) do
+ActiveRecord::Schema.define(version: 20171019020356) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "content"
+    t.text "content"
     t.bigint "user_id"
     t.bigint "post_id"
     t.datetime "created_at", null: false
