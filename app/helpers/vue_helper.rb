5ffcaba948e8344243ce5c00399dc220dd616119
linliulynn <linliulynn@gmail.com>
2017-11-01 18:11:21 -0400

    element-ui support theme-chalk

--- app/helpers/vue_helper.rb
+++ app/helpers/vue_helper.rb
@@ -2,7 +2,7 @@ module VueHelper
   def enable_vue_support
     content_for(:vue) do
       html = ''
-      html << stylesheet_link_tag('//unpkg.com/element-ui/lib/theme-default/index.css')
+      html << stylesheet_link_tag('//unpkg.com/element-ui/lib/theme-chalk/index.css')
       html << javascript_include_tag('//unpkg.com/vue/dist/vue.js')
       html << javascript_include_tag('//unpkg.com/element-ui/lib/index.js')
       html.html_safe
